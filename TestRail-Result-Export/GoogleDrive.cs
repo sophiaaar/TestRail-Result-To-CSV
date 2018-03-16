59659b2d722750d3974031bee47e0539ac041930
Sophia Clarke <sophiaclarke93@gmail.com>
2018-03-16 11:03:45 +0000

    working on windows

--- TestRail-Result-Export/GoogleDrive.cs
+++ TestRail-Result-Export/GoogleDrive.cs
@@ -48,27 +48,26 @@ namespace TestRailResultExport
                 ApplicationName = ApplicationName,
             });
 
-            // Define parameters of request.
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.PageSize = 10;
-            listRequest.Fields = "nextPageToken, files(id, name)";
+            //// Define parameters of request.
+            //FilesResource.ListRequest listRequest = service.Files.List();
+            //listRequest.PageSize = 10;
+            //listRequest.Fields = "nextPageToken, files(id, name)";
 
-            // List files.
-            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
-                .Files;
-            Console.WriteLine("Files:");
-            if (files != null && files.Count > 0)
-            {
-                foreach (var file in files)
-                {
-                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No files found.");
-            }
-            Console.Read();
+            //// List files.
+            //IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+            //Console.WriteLine("Files:");
+            //if (files != null && files.Count > 0)
+            //{
+            //    foreach (var file in files)
+            //    {
+            //        Console.WriteLine("{0} ({1})", file.Name, file.Id);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No files found.");
+            //}
+            //Console.Read();
 
             return service;
         }
